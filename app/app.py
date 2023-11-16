f3eb109bdc3f32426dca050b275e398f67f52fe2
Mikhail Moskovkin <anstane228@gmail.com>
2023-11-16 14:31:21 +0300

    Flask app

---
+++ app/app.py
@@ -0,0 +1,59 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, request
+from tinydb import TinyDB
+
+from .validation import get_template_type
+
+
+app = Flask(__name__)
+db = TinyDB('./templates/templates.json')
+
+log_formatter = logging.Formatter('%(asctime)s [%(levelname)s] - %(message)s')
+log_handler = RotatingFileHandler('file_log.log', maxBytes=100000, backupCount=1)
+log_handler.setFormatter(log_formatter)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(log_handler)
+
+
+def find_matching_template(request_data):
+    """Функция для поиска подходящих шаблонов внутри БД."""
+
+    templates = db.all()
+
+    for template in templates:
+        template_values = set(template.values()) - {template['name']} # Убираем дубликаты и поле name.
+        request_values = set(request_data.values()) # Собираем поля из request в set.
+
+        if template_values == request_values: # Сравнимаем шаблоны из БД и request.
+            return template['name']
+    
+    return None
+
+
+@app.route('/get_form', methods=['POST'])
+def get_form():
+    """Основная логика приложения."""
+
+    data = request.json
+    validated_data = get_template_type(data)
+    template_name = find_matching_template(validated_data)
+
+    try:
+        if template_name:
+            logger.info(f"Template found: {template_name}")
+            return template_name
+        else:
+            logger.info(f"Template not found <(_ _)>. Type of fields: "
+                        f"{', '.join(type for type in validated_data.values())}")
+            return validated_data
+
+    except Exception as error:
+        logger.error(f"Error: {error}")
+        return error, 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/validation.py
@@ -0,0 +1,56 @@
+import re
+from datetime import datetime
+
+
+def validate_email(email: str) -> bool:
+    """Простая валидация email с использованием регулярного выражения."""
+
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
+    return re.match(email_regex, email) is not None
+
+
+def validate_phone(phone: str) -> bool:
+    """Валидация номера телефона в формате +7XXXXXXXXXX."""
+
+    phone_regex = r'^\+7\d{10}$'
+
+    return re.match(phone_regex, phone) is not None
+
+
+def validate_date(date: str) -> bool:
+    """Валидация даты в форматах DD.MM.YYYY или YYYY-MM-DD."""
+
+    date_formats = ['%d.%m.%Y', '%Y-%m-%d']
+
+    for date_format in date_formats:
+        try:
+            datetime.strptime(date, date_format)
+            return True
+        except ValueError:
+            pass
+
+    return False
+
+
+def get_template_type(data):
+    """
+    Общий валидатор принимающий данные и отдающий тип поля.
+    """
+
+    types = {}
+
+    for field_name, value in data.items():
+        if validate_email(value):
+            types[field_name] = "email"
+
+        elif validate_phone(value):
+            types[field_name] = "phone"
+
+        elif validate_date(value):
+            types[field_name] = "date"
+
+        else:
+            types[field_name] = "text"
+
+    return types
